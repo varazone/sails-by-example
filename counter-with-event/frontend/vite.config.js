47ef19b378db234f51d5cbc04be92c07c6871298
navigaid <navigaid@gmail.com>
2024-09-27 02:32:17 +0000

    vite: add svgr plugin

--- counter-with-event/frontend/vite.config.js
+++ counter-with-event/frontend/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(), checker({ typescript: true })],
+  plugins: [react(), svgr(), nodePolyfills(), checker({ typescript: true })],
   resolve: { alias: { '@': '/src' } },
 })
