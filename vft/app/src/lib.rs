531623366d2da7cd904081cf6788a907801ebc43
Broken Pipe <navigaid@gmail.com>
2025-09-05 22:19:33 +0800

    migrate vft

--- vft/app/src/lib.rs
+++ vft/app/src/lib.rs
@@ -2,20 +2,18 @@
 #![allow(clippy::new_without_default)]
 
 use sails_rs::prelude::*;
-use services::vft;
-
-mod services;
+use vft_service::Service as VftService;
 
 pub struct Program(());
 
 #[program]
 impl Program {
     pub fn new(name: String, symbol: String, decimals: u8) -> Self {
-        <vft::Service>::seed(name, symbol, decimals);
+        <VftService>::seed(name, symbol, decimals);
         Self(())
     }
 
-    pub fn vft(&self) -> vft::Service {
-        vft::Service::new()
+    pub fn vft(&self) -> VftService {
+        VftService::new()
     }
 }
